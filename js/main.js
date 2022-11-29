0ddaf1db816e928c77d2cb865bf21cc9e0d5b79b
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-29 16:37:04 +0100

    Aggiunt amessaggio errore

--- js/main.js
+++ js/main.js
@@ -31,17 +31,21 @@
         addTask(){
 
         if(this.newTask.length < 5 || this.newTask === ""){
-            error = true
+            this.error = true
         }else{
             this.tasks.unshift(
                 {
                     text : this.newTask,
                 }
             )
+            this.error = false
         }
 
             // cancello l'input dell'utente al click
             this.newTask = ""
+
+            // tiportiamo l'errore a false per togliere la scritta error
+            
         },
     }
   }).mount('#app')
